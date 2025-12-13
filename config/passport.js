2814b2b34de037da4143c21f5fb2ebc4c48a1fa0
MuhammedFaazCC <faazmuhammed73@gmail.com>
2025-12-13 09:59:46 +0530

    changed google auth to become compatible with dns

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/userSchema');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:8080/auth/google/callback"
+  callbackURL: "http://starforge.autos/auth/google/callback" 
 }, async (accessToken, refreshToken, profile, done) => {
   console.log("✅ Raw Google profile:", JSON.stringify(profile, null, 2));
 
